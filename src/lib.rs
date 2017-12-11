c35b0e1cecdeb8ea6bc9e3ed2f0238e221dbd0bf
Mario Geiger <geiger.mario@gmail.com>
2017-12-11 22:55:38 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,212 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_prime_mr_works() {
+        assert!(is_prime_mr(11));
+        assert!(is_prime_mr(3));
+        assert!(is_prime_mr(2));
+    }
+
+    #[test]
+    fn next_prime_works() {
+        assert_eq!(next_prime(11), 11);
+        assert_eq!(next_prime(12), 13);
+    }
+
+    #[test]
+    fn previous_prime_works() {
+        assert_eq!(previous_prime(0), 0);
+        assert_eq!(previous_prime(1), 0);
+        assert_eq!(previous_prime(2), 2);
+        assert_eq!(previous_prime(3), 3);
+        assert_eq!(previous_prime(4), 3);
+        assert_eq!(previous_prime(5), 5);
+        assert_eq!(previous_prime(6), 5);
+        assert_eq!(previous_prime(7), 7);
+        assert_eq!(previous_prime(17), 17);
+        assert_eq!(previous_prime(18), 17);
+        assert_eq!(previous_prime(26), 23);
+    }
+
+    #[test]
+    fn prime_factors_works() {
+        assert_eq!(prime_factors(0), vec![]);
+        assert_eq!(prime_factors(1), vec![]);
+        assert_eq!(prime_factors(2), vec![(2, 1)]);
+        assert_eq!(prime_factors(2 * 2 * 7 * 13), vec![(2, 2), (7, 1), (13, 1)]);
+        assert_eq!(
+            prime_factors(37 * 37 * 37 * 7 * 13),
+            vec![(7, 1), (13, 1), (37, 3)]
+        );
+    }
+
+    #[test]
+    fn iterator_works() {
+        assert_eq!(primes(0).take(4).collect::<Vec<_>>(), vec![2, 3, 5, 7]);
+        assert_eq!(primes(0).find(|&x| x > 1000).unwrap(), 1009);
+    }
+}
+
+fn power(a: u64, n: u64, m: u64) -> u64 {
+    let mut power = a;
+    let mut result = 1;
+    let mut n = n;
+    while n > 0 {
+        if n & 1 > 0 {
+            result = (result * power) % m;
+        }
+        power = (power * power) % m;
+        n >>= 1;
+    }
+    result
+}
+
+fn witness(n: u64, s: u64, d: u64, a: u64) -> bool {
+    let mut x = power(a, d, n);
+    let mut y: u64 = 0;
+    let mut s = s;
+
+    while s > 0 {
+        y = (x * x) % n;
+        if y == 1 && x != 1 && x != n - 1 {
+            return false;
+        }
+        x = y;
+        s = s - 1;
+    }
+    y == 1
+}
+
+pub fn is_prime_mr(n: u64) -> bool {
+    if (n % 2 == 0 && n != 2) || (n < 2) || (n % 3 == 0 && n != 3) {
+        return false;
+    }
+    if n <= 3 {
+        return true;
+    }
+
+    let mut d = n / 2;
+    let mut s = 1;
+    while d % 2 == 0 {
+        d /= 2;
+        s += 1;
+    }
+
+    let w = |a: u64| -> bool { witness(n, s, d, a) };
+
+    if n < 2_047 {
+        w(2)
+    } else if n < 1_373_653 {
+        w(2) && w(3)
+    } else if n < 9_080_191 {
+        w(31) && w(73)
+    } else if n < 25_326_001 {
+        w(2) && w(3) && w(5)
+    } else if n < 3_215_031_751 {
+        w(2) && w(3) && w(5) && w(7)
+    } else if n < 4_759_123_141 {
+        w(2) && w(7) && w(61)
+    } else if n < 1_122_004_669_633 {
+        w(2) && w(13) && w(23) && w(1662803)
+    } else if n < 2_152_302_898_747 {
+        w(2) && w(3) && w(5) && w(7) && w(11)
+    } else if n < 3_474_749_660_383 {
+        w(2) && w(3) && w(5) && w(7) && w(11) && w(13)
+    } else if n < 341_550_071_728_321 {
+        w(2) && w(3) && w(5) && w(7) && w(11) && w(13) && w(17)
+    } else if n < 3_825_123_056_546_413_051 {
+        w(2) && w(3) && w(5) && w(7) && w(11) && w(13) && w(17) && w(19) && w(23)
+    } else if n < 3_825_123_056_546_413_051 {
+        w(2) && w(3) && w(5) && w(7) && w(11) && w(13) && w(17) && w(19) && w(23)
+    } else {
+        w(2) && w(3) && w(5) && w(7) && w(11) && w(13) && w(17) && w(19) && w(23) && w(29) && w(31)
+            && w(37)
+    }
+}
+
+pub fn next_prime(x: u64) -> u64 {
+    match x {
+        0 | 1 | 2 => {
+            return 2;
+        }
+        3 => {
+            return 3;
+        }
+        4 | 5 => return 5,
+        _ => {}
+    }
+    let k = x / 6;
+
+    let o = if x % 6 < 2 { 1 } else { 5 };
+
+    let mut x = 6 * k + o;
+    let mut i = (3 + o) / 2;
+    while !is_prime_mr(x) {
+        i ^= 6;
+        x += i;
+    }
+    x
+}
+
+pub fn previous_prime(mut x: u64) -> u64 {
+    match x {
+        0 | 1 => return 0,
+        2 => return 2,
+        3 | 4 => return 3,
+        _ => {}
+    }
+    if x % 2 == 0 {
+        x -= 1;
+    }
+
+    // x is odd
+    let (o, mut i) = if x % 6 == 5 { (5, 4) } else { (1, 2) };
+
+    x = (x / 6) * 6 + o;
+    while !is_prime_mr(x) {
+        x -= i;
+        i ^= 6;
+    }
+    x
+}
+
+pub fn prime_factors(mut x: u64) -> Vec<(u64, u32)> {
+    let mut xs = Vec::new();
+
+    let mut y = 2;
+    while x > 1 {
+        if is_prime_mr(x) {
+            xs.push((x, 1));
+            break;
+        }
+
+        let mut n = 0;
+        while x % y == 0 {
+            x /= y;
+            n += 1;
+        }
+        if n > 0 {
+            xs.push((y, n));
+        }
+        y = next_prime(y + 1);
+    }
+    xs
+}
+
+pub struct PrimeIter {
+    last: u64,
+}
+
+impl Iterator for PrimeIter {
+    type Item = u64;
+    fn next(&mut self) -> Option<u64> {
+        self.last = next_prime(self.last + 1);
+        Some(self.last)
+    }
+}
+
+pub fn primes(from: u64) -> PrimeIter {
+    PrimeIter { last: from }
+}
